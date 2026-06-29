3ab7462deff0a224bfbfd6d6fbeba92a3e2a0a11
LINKINEP <linkin61@HOTMAIL.FR>
2026-06-28 20:35:45 -1000

    Revert: restaure le CATALOG complet (les series US sont en VF sur stream4free.tv)

--- scripts/refresh_stream4free.py
+++ scripts/refresh_stream4free.py
@@ -7,14 +7,6 @@ NE FETCHE PAS les pages. Il genere le m3u depuis un catalogue hardcode.
 L'app ONYX resout le vrai flux m3u8 a la lecture via Stream4FreeResolver.
 
 Pour ajouter une chaine : ajouter une entree dans CATALOG ci-dessous.
-
-2026-06-28 (user "on a mis du contenu anglais sans faire expres dans la
-playlist, retire-les") : retirees Les Simpsons (EN), South Park US,
-Friends, Family Guy, The Big Bang Theory, HIMYM, The Office, Breaking Bad,
-Game of Thrones, Rick and Morty, Futurama, American Dad, Bob's Burgers,
-Archer, Sons of Anarchy, House MD, Scrubs, Seinfeld, Community, Cleveland
-Show, Workaholics, King of the Hill, Aqua Teen, Always Sunny, Poker. On
-garde Les Simpsons VF et South Park FR (= doublees francais).
 """
 
 import sys
@@ -31,15 +23,39 @@ CATALOG = [
     ("tf1-live-streaming",  "TF1",              f"{AVATAR}/tf1.jpg"),
     ("france-3-live",       "France 3",         f"{AVATAR}/france3.png"),
     ("france-4",            "France 4",         f"{AVATAR}/france4.png"),
-    # === EMISSIONS / SERIES EN BOUCLE 24/7 (uniquement contenu VF) ===
+    # === EMISSIONS / SERIES EN BOUCLE 24/7 ===
     ("kaamelott-hd",        "Kaamelott",        f"{AVATAR}/kaamelott.jpg"),
     ("simpsons-vf",         "Les Simpsons VF",  f"{AVATAR}/simpsons.jpg"),
+    ("the-simpsons",        "The Simpsons",     f"{AVATAR}/simpsons.jpg"),
     ("south-park-fr",       "South Park FR",    f"{AVATAR}/southpark.jpg"),
+    ("south-park-us",       "South Park US",    f"{AVATAR}/southpark.jpg"),
+    ("friends-live",        "Friends",          f"{AVATAR}/friends.jpg"),
+    ("family-guy-hd",       "Family Guy",       f"{AVATAR}/family.jpg"),
+    ("the-big-bang-theory",  "The Big Bang Theory", f"{AVATAR}/TBBT.jpg"),
+    ("himym",               "HIMYM",            f"{AVATAR}/himym.jpg"),
+    ("the-office",          "The Office",       f"{AVATAR}/theoffice.jpg"),
+    ("breaking-bad",        "Breaking Bad",     f"{AVATAR}/bb.jpg"),
+    ("game-of-thrones-hd",  "Game of Thrones",  f"{AVATAR}/game-of-thrones-logo.png"),
     ("the-walking-dead",    "The Walking Dead",  f"{AVATAR}/twd.jpg"),
+    ("rick-and-morty",      "Rick and Morty",   f"{AVATAR}/rickmorty.jpg"),
+    ("futurama",            "Futurama",         f"{AVATAR}/futurama.jpg"),
+    ("american-dad-hd",     "American Dad",     f"{AVATAR}/american_dad.gif"),
+    ("bobs-burgers",        "Bob's Burgers",    f"{AVATAR}/bobs.jpg"),
+    ("archer",              "Archer",           f"{AVATAR}/archer.jpg"),
+    ("sons-of-anarchy",     "Sons of Anarchy",  f"{AVATAR}/soa.jpg"),
+    ("house-md",            "House MD",         f"{AVATAR}/house.jpg"),
+    ("scrubs",              "Scrubs",           f"{AVATAR}/scrubs.jpg"),
+    ("seinfeld",            "Seinfeld",         f"{AVATAR}/seinfeld.jpg"),
     ("h-integrale",         "H Integrale",      f"{AVATAR}/h.jpg"),
     ("camera-cafe-stream",  "Camera Cafe",      f"{AVATAR}/cameracafe.jpg"),
+    ("greendale-college",   "Community",        f"{AVATAR}/community.jpg"),
+    ("the-cleveland-show",  "Cleveland Show",   f"{AVATAR}/cleveland.jpg"),
+    ("workaholics",         "Workaholics",      f"{AVATAR}/workaholics.jpg"),
+    ("king-of-the-hill",    "King of the Hill", f"{AVATAR}/kinghill.jpg"),
+    ("aqua-teen-hunger-force", "Aqua Teen",     f"{AVATAR}/aqua.jpg"),
     ("triptank",            "Triptank",         f"{AVATAR}/triptank.jpg"),
     ("70-show",             "That 70s Show",    f"{AVATAR}/70show.jpg"),
+    ("always-sunny-in-philadelphia", "Always Sunny", f"{AVATAR}/sunny.jpg"),
     ("stargate-sg1-sga",    "Stargate",         f"{AVATAR}/stargate.jpg"),
     ("dragonball-dbz",      "Dragon Ball Z",    f"{AVATAR}/dragonball.jpg"),
     ("ddc",                 "DDC",              ""),
@@ -48,6 +64,7 @@ CATALOG = [
     ("special-investigation","Special Investigation", f"{AVATAR}/specialinvestigation.jpg"),
     ("l-univers-et-ses-mysteres", "L'Univers",  f"{AVATAR}/univers.jpg"),
     ("tv-sciences",         "TV Sciences",      f"{AVATAR}/science.jpg"),
+    ("poker-stream",        "Poker",            ""),
 ]
 
 
